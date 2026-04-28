5046cad7f8cc5ee3900b4f704819753797bce843
Viktoriya <viktoriya@Viktoriyas-MacBook-Pro.local>
2026-04-28 11:51:27 +0500

    cursor fixed smth

--- Schedule.java
+++ Schedule.java
@@ -6,21 +6,30 @@ class Schedule extends Booking implements GetItems, SetItems {
     private String semester;
     
     public Schedule(String course, String instructor, Classroom classroom, String time) {
-        super(course);
+        super(time, instructor, classroom != null ? classroom.getRoomNumber() : "", 0);
         this.course = course;
+        this.instructor = instructor;
+        this.host = instructor;
         this.classroom = classroom;
         this.time = time;
+        this.date = time;
         this.semester = "";
     }
 
     public String getCourse() { return course; }
     public void setCourse(String course) { this.course = course; }
 
-    public String getInstructor() { return host; }
-    public void setInstructor(String instructor) { this.host = instructor; }
+    public String getInstructor() { return instructor; }
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+        this.host = instructor;
+    }
 
     public Classroom getClassroom() { return classroom; }
-    public void setClassroom(Classroom classroom) { this.classroom = classroom; }
+    public void setClassroom(Classroom classroom) {
+        this.classroom = classroom;
+        this.location = classroom != null ? classroom.getRoomNumber() : "";
+    }
 
     public String getTime() { return time; }
     public void setTime(String time) {
@@ -31,7 +40,6 @@ class Schedule extends Booking implements GetItems, SetItems {
     public String getSemester() { return semester; }
     public void setSemester(String semester) { this.semester = semester; }
 
-    @Override
     public String getName() {
         return "Shedule name is: " + course;
     }
